913d5703d7e7deea93d8f17d9df6c1c2140601da
Alex Karius <2371042+arkari-us@users.noreply.github.com>
2021-04-22 00:04:09 -0500

    Cleanup from default react app, very early work on components for main page

--- src/App.js
+++ src/App.js
@@ -1,25 +1,63 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends React.Component {
+  state = {
+    recipes: [
+      {
+        id: 1,
+        name: 'Kale and White Bean Skillet',
+        ingredients: [
+          { quantity: 2, unit: 'can', ingredient: 'Cannelini beans' },
+          { quantity: 1 / 2, unit: 'jar', ingredient: 'Sun-dried tomatoes' },
+          { quantity: 6, unit: 'oz', ingredient: "Kale" }
+        ],
+        instructions: 'heat ingredients in skillet',
+        tags: [String]
+      },
+      {
+        id: 2,
+        name: 'Tuna Salad',
+        ingredients: [
+          { quantity: 12, unit: 'oz', ingredient: 'Tuna' },
+          { quantity: 2, unit: '', ingredient: 'Dill pickles' },
+          { quantity: 1 / 4, unit: '', ingredient: 'Red onion' },
+          { quantity: 1 / 2, unit: 'cup', ingredient: 'Mayonnaise' }
+        ],
+        instructions: 'mix ingredients',
+        tags: [String]
+      }
+    ],
+    currentRecipe: {
+      name: '',
+      ingredients: []
+    }
+  }
+
+  swap = (recipe) => {
+    this.setState({currentRecipe: recipe});
+  }
+
+  render() {
+    return (
+      <>
+        <ul>
+          {this.state.recipes.map(recipe => {
+            return (
+              <li onClick={() => this.swap(recipe)}>{recipe.name}</li>
+            );
+          })}
+        </ul>
+        <div>
+          <h4>{this.state.currentRecipe.name || 'No recipe selected'}</h4>
+          <ul>
+          {this.state.currentRecipe.ingredients.map(
+            ingredient => <li>{ingredient.quantity} {ingredient.unit} {ingredient.ingredient}</li>
+          )}
+          </ul>
+        </div>
+      </>
+    );
+  }
 }
 
 export default App;

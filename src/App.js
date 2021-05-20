6a745ee68e9bfdae5c577854c809fabe07771849
Alex Karius <2371042+arkari-us@users.noreply.github.com>
2021-05-19 21:32:24 -0500

    Adjustments to make recipe data show up correctly--will need refactoring later

--- src/App.js
+++ src/App.js
@@ -4,25 +4,25 @@ import DataInterface from './data/DataInterface';
 class RecipeList extends React.Component {
   render(props) {
     return (
-      <ul>
-        {this.props.recipes.map(recipe => {
+      <ul style={{visibility: this.props.recipes[0] ? 'visible' : 'hidden'}}>
+        {this.props.recipes?.map(recipe => {
           return (
             <li onClick={() => this.props.changeRecipe(recipe)} key={recipe.id}>{recipe.name}</li>
           );
         })}
       </ul>
-    )
+    );
   }
 }
 
 class RecipeDisplay extends React.Component {
   render(props) {
     return (
-      <div>
-        <h4>{this.props.recipe.name}</h4>
+      <div style={{visibility: this.props.recipe?.name ? 'visible' : 'hidden'}}>
+        <h4>{this.props.recipe?.name}</h4>
         <ul>
-          {this.props.recipe.ingredients.map(
-            ingredient => <li>{ingredient.quantity} {ingredient.unit} {ingredient.ingredient}</li>
+          {this.props.recipe?.ingredients?.map(
+            ingredient => <li key={ingredient.id}>{ingredient.quantity} {ingredient.unit} {ingredient.ingredient}</li>
           )}
         </ul>
       </div>
@@ -32,20 +32,16 @@ class RecipeDisplay extends React.Component {
 
 function App(props) {
   const [recipes, setRecipes] = useState([]);
-  const [currentRecipe, setCurrentRecipe] = useState({
-    name: 'no recipe selected',
-    ingredients: []
-  });
+  const [currentRecipe, setCurrentRecipe] = useState({});
 
   useEffect(() => {
     DataInterface.getRecipes()
       .then((res) => {
-        console.log(res);
         if (res) setRecipes(res);
       });
   }, []);
 
-  function changeRecipe (recipe) {
+  function changeRecipe(recipe) {
     setCurrentRecipe(recipe);
   }
 
